46c9e29351222d404a905904ad73af3533bb855c
RocioMartinez <rym5411@gmail.com>
2023-06-07 19:30:18 -0400

    added state to App, installed funtion

--- src/App.js
+++ src/App.js
@@ -5,14 +5,29 @@ import React from 'react';
 import Header from './Header'
 import Footer from './Footer';
 import Main from './Main';
+import data from './data/data.json';
 
 // #2: CREATE CLASS COMPONENT
 class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      rainbows: ''
+    }
+  }
+
+  addRainbows = () => {
+    this.setState({
+      rainbows: this.state.rainbows + '🌈'
+    })
+  }
+
   render(){
     return(
       <>
-      <Header/>
-      <Main/>
+      <Header rainbows={this.state.rainbows}/>
+      <Main data={data} addRainbows={this.addRainbows} />
       <Footer/>
       </>
     )
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -22,10 +22,14 @@ class HornedBeast extends React.Component{
     <article>
     <Card style={{ width: '18rem' }}>
     <Card.Body>
-    <Card.Title>Name: {this.props.title}</Card.Title>
+    <Card.Title onClick={this.handleLike}>Name: {this.props.title}</Card.Title>
     <Card.Text> 🦄 {this.state.likes} Favorites </Card.Text>
-    <Card.Text>Pick your favorite beast by clicking its image!</Card.Text>
-    <Card.Img onClick={this.handleLike} src={this.props.image_url} alt={this.props.description} title={this.props.title}/>
+    <Card.Text>Pick your favorite beast by clicking its name! Click the image that makes you happy to get celebration rainbows!</Card.Text>
+    <Card.Img 
+     onClick={this.props.addRainbows}
+     src={this.props.image_url} 
+     alt={this.props.description} 
+     title={this.props.title}/>
     <Card.Text>Description: {this.props.description}</Card.Text>
     </Card.Body>
     </Card>
