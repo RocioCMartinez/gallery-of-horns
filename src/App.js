a935a50e9e7cd88eeb281b13bf907dd95ab703a1
RocioMartinez <rym5411@gmail.com>
2023-06-08 19:45:48 -0400

    implemented use of form and filter

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 
 // #1: IMPORT
 import React from 'react';
-import Header from './Header'
+import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
 import data from './data/data.json';
 import SelectedBeast from './SelectedBeast';
+import Form from 'react-bootstrap/Form';
+
 
 // #2: CREATE CLASS COMPONENT
 class App extends React.Component{
@@ -18,7 +20,8 @@ class App extends React.Component{
       showModal: false,
       beastTitle: '',
       beastImg: '',
-      beastDesription: ''
+      beastDesription: '',
+      sortedData: data
     }
   }
 
@@ -32,13 +35,13 @@ class App extends React.Component{
       beastDesription: description
   
     })
-   }
+  }
   
-   handleCloseModal = () => {
+  handleCloseModal = () => {
     this.setState({
       showModal: false
     })
-   }
+  }
 
   addRainbows = () => {
     this.setState({
@@ -46,20 +49,60 @@ class App extends React.Component{
     })
   }
 
+  handleChange = (event) => {
+    let selected = parseInt(event.target.value);
+    
+    if (selected === 1){
+      let newData = data.filter(element => element.horns === 1);
+      this.setState({
+        sortedData: newData
+      })
+    } else if (selected === 2){
+      let newData = data.filter(element => element.horns === 2);
+      this.setState({
+        sortedData: newData
+      })
+    } else if (selected === 3){
+      let newData = data.filter(element => element.horns === 3);
+      this.setState({
+        sortedData: newData
+      })
+    } else if (selected === 100){
+      let newData = data.filter(element => element.horns === 100);
+      this.setState({
+        sortedData: newData
+      })
+    } else if (selected === 0){
+      this.setState({
+        sortedData: data
+      })
+    }
+    
+  }
+
   render(){
     return(
       <>
       <Header rainbows={this.state.rainbows}/>
-      <Main data={data} addRainbows={this.addRainbows}  handleOpenModal={this.handleOpenModal}/>
+      <Form.Select onChange={this.handleChange} aria-label="hornedForm">
+      <option>Filter beasts by horns 🎺</option>
+      <option value="0">All</option>
+      <option value="1">1 horn</option>
+      <option value="2">2 horns</option>
+      <option value="3">3 horns</option>
+      <option value="100">100 horns</option>
+      </Form.Select>
+      
+
+      <Main data={this.state.sortedData} addRainbows={this.addRainbows}  handleOpenModal={this.handleOpenModal}/>
       <SelectedBeast  handleCloseModal={this.handleCloseModal} showModal={this.state.showModal} beastTitle={this.state.beastTitle} beastImg={this.state.beastImg} beastDescription={this.state.beastDesription} beastObj={data}>
       </SelectedBeast>
-
     
       <Footer/>
       </>
     )
   }
-}
+};
 
 // #3: EXPORT THE CLASS
 export default App;
